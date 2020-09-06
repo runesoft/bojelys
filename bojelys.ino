bc32d8a718f45f70c98cdf45a19ca372852d0eb1
Rune Juhl-Petersen <rune@juhl-petersen.net>
2020-09-06 19:42:32 +0200

    Added some documentation

--- bojelys.ino
+++ bojelys.ino
@@ -3,37 +3,36 @@
 #define NUM_LEDS 50
 #define DATA_PIN 12
 
-#define NUM_PROGS 6
-#define NUM_COLORS 3
+#define NUM_PROGS 6 //number of leds that can be lit at the same time
+#define NUM_COLORS 3 //number of colors to use
 
 CRGB leds[NUM_LEDS];
-int delays [NUM_PROGS];
-int progs [NUM_PROGS];
+int delays [NUM_PROGS]; //for every program, counter to figure out when to do something next time.
+int progs [NUM_PROGS];  //Current led that each program is interacting with 
 CRGB colors[]= {CRGB::Red,CRGB::Green,CRGB::Blue, CRGB::Yellow,CRGB::Purple,CRGB::White};
 
 
 void setup() { 
   FastLED.addLeds<WS2811, DATA_PIN, RGB>(leds, NUM_LEDS);
-  for(int i =0; i<NUM_PROGS;i++){
+  for(int i =0; i<NUM_PROGS;i++){ //initiate programs with a delay so they start gradually
       delays[i]= random(30);
   }
   int nums=3;
-  for(int i =0; i< NUM_LEDS+(nums*(NUM_COLORS+1)); i++){
-    for(int color=0; color<=NUM_COLORS; color++){
-      int current = i - (color*nums);
-      if(current>=0 && current<NUM_LEDS){
+  //control run to test that all lights work
+  for(int i =0; i< NUM_LEDS+(nums*(NUM_COLORS+1)); i++){ //for every LED + the windows lighting up
+    for(int color=0; color<=NUM_COLORS; color++){ //for every color+1
+      int current = i - (color*nums);// look back from current
+      if(current>=0 && current<NUM_LEDS){ //only do stuff if it is an actual LED
         if(color<NUM_COLORS){
           leds[current]= colors[color];
-        } else{
+        } else{// turn off LED if not a color
           leds[current]= CRGB::Black;
-          
         } 
       }
     }  
     FastLED.show();
-  delay(100);
-  }
-  
+    delay(100);
+  }  
 }
 
 bool isPartOf(int led){
@@ -49,17 +48,16 @@ void loop() {
     delays[i]= delays[i]-1;
     if(delays[i]<0){
       int led = progs[i];
-      if(led<NUM_LEDS && led>=0){
-        //turn of led
+      if(led<NUM_LEDS && led>=0){ //turn of led
         leds[progs[i]]=CRGB::Black;
         progs[i]= NUM_LEDS + 1;
         delays[i]= 4+random(15);
       }else{
-        int nxtled = random(NUM_LEDS);
-        if(!isPartOf(nxtled)){
-          progs[i] = nxtled; 
-          leds[progs[i]]=colors[random(NUM_COLORS)];
-          delays[i]= 4+random(15);
+        int nxtled = random(NUM_LEDS); //pick a random next LED
+        if(!isPartOf(nxtled)){ // if not already active
+          progs[i] = nxtled; //store led number in program
+          leds[progs[i]]=colors[random(NUM_COLORS)]; //set random color
+          delays[i]= 4+random(15); // set random delay until it is turned off
         }
       } 
     }  
