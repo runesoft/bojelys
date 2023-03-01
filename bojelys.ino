b60f1b0ae1d20fcba753eee172fa9b8ce813dfe2
Rune Juhl-Petersen <rune@juhl-petersen.net>
2023-03-01 17:33:45 +0100

    nu med faders

--- bojelys.ino
+++ bojelys.ino
@@ -3,36 +3,40 @@
 #define NUM_LEDS 50
 #define DATA_PIN 12
 
-#define NUM_PROGS 6 //number of leds that can be lit at the same time
+#define NUM_PROGS 15 //number of leds that can be lit at the same time
 #define NUM_COLORS 3 //number of colors to use
+
+#define OFF 0
+#define UP 1
+#define ON 2
+#define DOWN 3
+
+
+
+#define FADETIME 2000
+#define FPS 60
+#define DELAY 1000/FPS
+#define FADESTEP 255/((FADETIME/1000)*FPS)
+#define MIN_PERIOD 2000
+#define MAX_PERIOD 15000
+#define FLEX_PERIOD MAX_PERIOD-MIN_PERIOD
+
 
 CRGB leds[NUM_LEDS];
 int delays [NUM_PROGS]; //for every program, counter to figure out when to do something next time.
 int progs [NUM_PROGS];  //Current led that each program is interacting with 
+char states [NUM_PROGS];
+int hues [NUM_PROGS];
+
 CRGB colors[]= {CRGB::Red,CRGB::Green,CRGB::Blue, CRGB::Yellow,CRGB::Purple,CRGB::White};
 
-
 void setup() { 
-  FastLED.addLeds<WS2811, DATA_PIN, RGB>(leds, NUM_LEDS);
-  for(int i =0; i<NUM_PROGS;i++){ //initiate programs with a delay so they start gradually
-      delays[i]= random(30);
-  }
-  int nums=3;
-  //control run to test that all lights work
-  for(int i =0; i< NUM_LEDS+(nums*(NUM_COLORS+1)); i++){ //for every LED + the windows lighting up
-    for(int color=0; color<=NUM_COLORS; color++){ //for every color+1
-      int current = i - (color*nums);// look back from current
-      if(current>=0 && current<NUM_LEDS){ //only do stuff if it is an actual LED
-        if(color<NUM_COLORS){
-          leds[current]= colors[color];
-        } else{// turn off LED if not a color
-          leds[current]= CRGB::Black;
-        } 
-      }
-    }  
-    FastLED.show();
-    delay(100);
-  }  
+  FastLED.addLeds<WS2811, DATA_PIN, RGB>(leds, NUM_LEDS); 
+    Serial.begin(115200); // open the serial port at 9600 bps:
+ for(int i =0;i<NUM_PROGS;i++){
+  states[i]=OFF;
+ }
+ //Serial.print(FADESTEP);
 }
 
 bool isPartOf(int led){
@@ -45,23 +49,59 @@ bool isPartOf(int led){
 }
 void loop() { 
   for(int i =0; i<NUM_PROGS;i++){
-    delays[i]= delays[i]-1;
-    if(delays[i]<0){
-      int led = progs[i];
-      if(led<NUM_LEDS && led>=0){ //turn of led
+    
+    delays[i]-=DELAY;
+    if(states[i]==UP){
+      
+      if(hues[i]>(255-FADESTEP)){
+        Serial.print("I");
+
+        //leds[progs[i]].maximizeBrightness(255);
+        states[i]=ON;
+        delays[i]= MIN_PERIOD + random(FLEX_PERIOD); // set random delay until it is turned off
+      }else{
+         // Serial.print("(");
+         // Serial.print( hues[i]);
+         // Serial.print(")");
+        hues[i]+=FADESTEP;
+        leds[progs[i]].maximizeBrightness(hues[i]);
+      }  
+    } else if(states[i]==DOWN){
+      if(hues[i]<FADESTEP){
+        Serial.print("O");
+        
+        //leds[progs[i]].maximizeBrightness(0);
         leds[progs[i]]=CRGB::Black;
         progs[i]= NUM_LEDS + 1;
-        delays[i]= 4+random(15);
-      }else{
+        states[i]=OFF;
+        delays[i]= MIN_PERIOD+random(FLEX_PERIOD); // set random delay until it is turned on
+      } else {
+        Serial.print("-");
+        
+        hues[i]-=FADESTEP;
+        leds[progs[i]].maximizeBrightness(hues[i]);
+
+      }
+    }
+    else if(delays[i]<0){
+      int led = progs[i];
+      if(states[i]==ON){ //turn of led
+        states[i]=DOWN;
+       }else{
         int nxtled = random(NUM_LEDS); //pick a random next LED
         if(!isPartOf(nxtled)){ // if not already active
+          Serial.print("shifting to up");
+          states[i]=UP;
           progs[i] = nxtled; //store led number in program
+          Serial.print(random(NUM_COLORS));
           leds[progs[i]]=colors[random(NUM_COLORS)]; //set random color
-          delays[i]= 4+random(15); // set random delay until it is turned off
+          hues[i]=1;
+          leds[progs[i]].maximizeBrightness(FADESTEP);
+          //leds[progs[i]].fadeLightBy(1);
         }
       } 
     }  
   }
   FastLED.show();
-  delay(500);
+  delay(DELAY);
 }
