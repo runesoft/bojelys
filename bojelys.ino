65bb813aec51aeef8b8a759a09e1b83916d16085
Rune Juhl-Petersen <rune@juhl-petersen.net>
2023-03-02 21:36:07 +0100

    with struct

--- bojelys.ino
+++ bojelys.ino
@@ -1,6 +1,6 @@
 #include <FastLED.h>
 
-#define NUM_LEDS 50
+#define NUM_LEDS 100
 #define DATA_PIN 12
 
 #define NUM_PROGS 15 //number of leds that can be lit at the same time
@@ -23,84 +23,75 @@
 
 
 CRGB leds[NUM_LEDS];
-int delays [NUM_PROGS]; //for every program, counter to figure out when to do something next time.
-int progs [NUM_PROGS];  //Current led that each program is interacting with 
-char states [NUM_PROGS];
-int hues [NUM_PROGS];
+struct Program{
+  int delay;
+  int led;
+  char state;
+  int hue;
+};
+Program programs [NUM_PROGS];
 
 CRGB colors[]= {CRGB::Red,CRGB::Green,CRGB::Blue, CRGB::Yellow,CRGB::Purple,CRGB::White};
 
 void setup() { 
   FastLED.addLeds<WS2811, DATA_PIN, RGB>(leds, NUM_LEDS); 
-    Serial.begin(115200); // open the serial port at 9600 bps:
- for(int i =0;i<NUM_PROGS;i++){
-  states[i]=OFF;
- }
- //Serial.print(FADESTEP);
+  Serial.begin(115200); // open the serial port at 9600 bps:
+  for(int i =0;i<NUM_PROGS;i++){
+    programs[i]={0,NUM_LEDS+1,OFF,0};
+  }
 }
 
 bool isPartOf(int led){
   for(int i = 0;i<NUM_PROGS;i++){
-    if(progs[i]==led){
+    if(programs[i].led==led){
       return true;
     }
   }
   return false;  
 }
 void loop() { 
+  
   for(int i =0; i<NUM_PROGS;i++){
-    
-    delays[i]-=DELAY;
-    if(states[i]==UP){
-      
-      if(hues[i]>(255-FADESTEP)){
-        Serial.print("I");
-
-        //leds[progs[i]].maximizeBrightness(255);
-        states[i]=ON;
-        delays[i]= MIN_PERIOD + random(FLEX_PERIOD); // set random delay until it is turned off
+    Program current=programs[i];
+    current.delay-=DELAY;
+    if(current.state==UP){
+      if(current.hue>(255-FADESTEP)){
+        current.state =ON;
+        current.delay = MIN_PERIOD + random(FLEX_PERIOD); // set random delay until it is turned off
       }else{
-         // Serial.print("(");
-         // Serial.print( hues[i]);
-         // Serial.print(")");
-        hues[i]+=FADESTEP;
-        leds[progs[i]].maximizeBrightness(hues[i]);
+        current.hue+=FADESTEP;
+        //set color?
+        leds[current.led].maximizeBrightness(current.hue);
       }  
-    } else if(states[i]==DOWN){
-      if(hues[i]<FADESTEP){
-        Serial.print("O");
-        
+    } else if(current.state==DOWN){
+      if(current.hue<FADESTEP){  
         //leds[progs[i]].maximizeBrightness(0);
-        leds[progs[i]]=CRGB::Black;
-        progs[i]= NUM_LEDS + 1;
-        states[i]=OFF;
-        delays[i]= MIN_PERIOD+random(FLEX_PERIOD); // set random delay until it is turned on
+        leds[current.led] = CRGB::Black;
+        current.led = NUM_LEDS + 1;
+        current.state = OFF;
+        current.delay = MIN_PERIOD+random(FLEX_PERIOD); // set random delay until it is turned on
       } else {
-        Serial.print("-");
-        
-        hues[i]-=FADESTEP;
-        leds[progs[i]].maximizeBrightness(hues[i]);
+        current.hue-=FADESTEP;
+        leds[current.led].maximizeBrightness(current.hue);
 
       }
     }
-    else if(delays[i]<0){
-      int led = progs[i];
-      if(states[i]==ON){ //turn of led
-        states[i]=DOWN;
+    else if(current.delay<0){
+      if(current.state==ON){ //turn of led
+        current.state=DOWN;
        }else{
         int nxtled = random(NUM_LEDS); //pick a random next LED
         if(!isPartOf(nxtled)){ // if not already active
-          Serial.print("shifting to up");
-          states[i]=UP;
-          progs[i] = nxtled; //store led number in program
-          Serial.print(random(NUM_COLORS));
-          leds[progs[i]]=colors[random(NUM_COLORS)]; //set random color
-          hues[i]=1;
-          leds[progs[i]].maximizeBrightness(FADESTEP);
-          //leds[progs[i]].fadeLightBy(1);
+          current.state=UP;
+          current.led = nxtled; //store led number in program
+          leds[current.led]=colors[random(NUM_COLORS)]; //set random color
+          current.hue=FADESTEP;
+          leds[current.led].maximizeBrightness(FADESTEP);
         }
       } 
-    }  
+    }
+    programs[i]=current;
+    
   }
   FastLED.show();
   delay(DELAY);
